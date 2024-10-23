d50ff7ee44d506618a7a0e5733dbe490df452cf5
mdnv <iliyarmidinov11@gmail.com>
2024-10-23 21:55:25 +0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+document.getElementById("react-progress").style.width = "00%";
+document.getElementById("html-css-progress").style.width = "00%";
+document.getElementById("javascript-progress").style.width = "00%";
+
